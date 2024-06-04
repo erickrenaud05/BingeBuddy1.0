dbeb5fea094f2b405798033034368fe135028ab4
Erick Renaud <renaud_junior@outlook.com>
2024-06-04 18:48:38 -0400

    added functions to display movie cards
    Added two functions and a starting point. The first function is called displayMovieCards which will take in a movie and dynamically append html to the movie section. The second function called createMovieCards will retrieve an array of movies, and use displayMovieCards based on this array. The starting point for my code will use the custom event I made to ensure the code doesnt run unless the movies are in localStorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 const movieTitles = [];
 var movies = JSON.parse(localStorage.getItem('top100'));
 const event = new Event("loaded");
+const movieCardArea = $('#movieCardArea');
 
 if(!movies){
     const url = 'https://imdb-top-100-movies.p.rapidapi.com/';
@@ -23,3 +24,38 @@ if(!movies){
 } else {
     document.dispatchEvent(event);
 }
+
+function displayMovieCards(movie) {
+    
+    const movieCardEl = $('<div>').addClass('movieCard row col-lg-2 col-md-3 col-sm-4 m-2 align-content-start');
+    const movieCardBody = $('<div>').addClass('movieCard justify-content-center card-body bg-dark text-white').attr('style', 'height: 125px');
+    const movieCardTitle = $('<h5>').addClass('movieCard card-title user-select-none').text(movie.title);
+    movieCardBody.css('background-image', 'url('+movie.image+')');
+    movieCardBody.attr('id', movie.title);
+
+    movieCardEl.append(movieCardBody, movieCardTitle);
+    movieCardArea.append(movieCardEl);
+
+    $('.movieCard').click(function(event){
+        displayCardDetails(event.target.id)
+    })
+}
+
+function createMovieCards(randomizeSelection) {
+    movieCardArea.empty();
+    movieMatch = randomizeSelection;
+
+    for (let i = 0; i < movieMatch.length; i++) {    
+        displayMovieCards(movieMatch[i]);
+    }
+}
+
+$(document).ready(function(){
+    document.addEventListener(
+        "loaded",
+        (e) => {
+            
+        },
+        false,
+    );
+})
